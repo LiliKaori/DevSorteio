2d3c0c2c8855f84a050c869499cbe61cf5fe61d7
LiliKaori <lilikaori10@gmail.com>
2023-05-27 19:44:57 -0300

    Criar função checar se número existe

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 "use strict";
+const input = document.getElementsByTagName("input");
 const inputMin = document.getElementById("numMin");
 const inputMax = document.getElementById("numMax");
+const button = document.getElementsByTagName("button");
+function checkNumbers() {
+    const min = parseInt(inputMin.value);
+    const max = parseInt(inputMax.value);
+    if (!min || !max) {
+        button[0].disabled = true;
+    }
+    else {
+        button[0].disabled = false;
+    }
+    console.log(button[0]);
+}
+checkNumbers();
+for (let i = 0; i < input.length; i++) {
+    input[i].addEventListener("keyup", checkNumbers, false);
+}
 function raffle() {
     const min = parseInt(inputMin.value);
     const max = parseInt(inputMax.value);
--- script.ts
+++ script.ts
@@ -1,7 +1,25 @@
+    const input = document.getElementsByTagName("input") as HTMLCollectionOf<HTMLInputElement>
     const inputMin = document.getElementById("numMin") as HTMLInputElement
     const inputMax= document.getElementById("numMax") as HTMLInputElement
+    const button = document.getElementsByTagName("button") as HTMLCollectionOf<HTMLButtonElement>
 
-function raffle(){
+function checkNumbers(){
+    const min : number = parseInt(inputMin.value)
+    const max : number = parseInt(inputMax.value)
+
+    if(!min || !max){
+        button[0].disabled = true
+    } else {
+        button[0].disabled = false
+    }
+    console.log(button[0])
+}
+checkNumbers()
+
+for(let i=0; i<input.length; i++){input[i].addEventListener("keyup", checkNumbers, false)}
+
+
+function raffle(): void{
     const min : number = parseInt(inputMin.value)
     const max : number = parseInt(inputMax.value)
     

761fba822d653b9f1ea4bb1e086e341e11eea6de
LiliKaori <lilikaori10@gmail.com>
2023-05-28 19:13:29 -0300

    Função sortear vários números e alerta de erro

--- script.js
+++ script.js
@@ -6,32 +6,51 @@ const divResult = document.getElementsByClassName("result");
 function filterInput(i) {
     const filteredValue = input[i].value.replaceAll(/[\D]/gim, "").replaceAll(/^[0]+/gim, "");
     input[i].value = filteredValue;
+    checkNumbers();
 }
 function checkNumbers() {
     const empty = arrayInput.find(input => input.value === "");
     button.disabled = empty ? true : false;
-}
-function raffle() {
     const values = arrayInput.map((input) => parseInt(input.value));
-    // const quantity = values[0]
+    const quantity = values[0];
     const numbers = values.slice(1);
     const min = Math.min(...numbers);
     const max = Math.max(...numbers);
-    const result = Math.floor(Math.random() * (min - max) + max);
+    if (quantity && quantity > max - min + 1) {
+        const alert = document.getElementById("alert");
+        alert.innerHTML = `Só pode sortear até ${max - min + 1} ${max - min + 1 == 1 ? "número" : "números"}`;
+        button.disabled = true;
+    }
+    else {
+        const alert = document.getElementById("alert");
+        alert.innerHTML = "";
+        button.disabled = false;
+    }
+}
+function raffle() {
+    const values = arrayInput.map((input) => parseInt(input.value));
+    const quantity = values[0];
+    const numbers = values.slice(1);
+    const min = Math.min(...numbers);
+    const max = Math.max(...numbers);
+    let result = [];
+    while (result.length < quantity) {
+        const sort = Math.floor((Math.random() * (max - min + 1)) + min);
+        if (result.indexOf(` ${sort.toString()}`) === -1) {
+            result.push(` ${sort.toString()}`);
+        }
+    }
+    // const result : number = Math.floor(Math.random() * (min - max) + max)
     input[1].value = min.toString();
     input[2].value = max.toString();
     divResult[0].innerHTML += `
         <div>
             <h2>${result}</h2>
-            <p class="textResult">1º resultado</p>
+            <p class="textResult">${quantity} ${quantity === 1 ? "número sorteado" : "números sorteados"} de ${min} até ${max}.</p>
         </div>
     `;
 }
-checkNumbers();
 for (let i = 0; i < input.length; i++) {
     input[i].addEventListener("keyup", () => { filterInput(i); }, false);
-}
-for (let i = 0; i < input.length; i++) {
-    input[i].addEventListener("keyup", checkNumbers, false);
 }
 button.addEventListener("click", raffle, false);
--- script.ts
+++ script.ts
@@ -6,23 +6,49 @@
 function filterInput(i:number) : void {
     const filteredValue : string = input[i].value.replaceAll(/[\D]/gim,"").replaceAll(/^[0]+/gim, "")   
     input[i].value = filteredValue
+    
+    checkNumbers()
 }
 
 function checkNumbers(): void{
     const empty = arrayInput.find(input=>input.value==="")
-
     button.disabled = empty ? true : false
-}
-
-function raffle(): void{    
     const values = arrayInput.map((input)=>parseInt(input.value))
-    // const quantity = values[0]
+    const quantity = values[0]
     const numbers = values.slice(1)
     
     const min : number = Math.min(...numbers)
     const max : number = Math.max(...numbers)
 
-    const result : number = Math.floor(Math.random() * (min - max) + max)
+    if(quantity && quantity>max-min+1){
+        const alert = document.getElementById("alert") as HTMLParagraphElement
+        alert.innerHTML=`Só pode sortear até ${max-min+1} ${max-min+1==1?"número":"números"}`
+        button.disabled = true
+    }else{
+        const alert = document.getElementById("alert") as HTMLParagraphElement
+        alert.innerHTML=""
+        button.disabled = false
+    }
+}
+
+function raffle(): void{    
+    const values = arrayInput.map((input)=>parseInt(input.value))
+    const quantity = values[0]
+    const numbers = values.slice(1)
+    
+    const min : number = Math.min(...numbers)
+    const max : number = Math.max(...numbers)
+
+    let result : string[] = []
+
+    while(result.length<quantity){
+       const sort = Math.floor((Math.random() * (max - min + 1)) + min)
+       
+       if(result.indexOf(` ${sort.toString()}`)===-1)
+       {result.push(` ${sort.toString()}`)}
+    }
+
+    // const result : number = Math.floor(Math.random() * (min - max) + max)
 
     input[1].value = min.toString()
     input[2].value = max.toString()
@@ -30,12 +56,10 @@ function raffle(): void{
     divResult[0].innerHTML += `
         <div>
             <h2>${result}</h2>
-            <p class="textResult">1º resultado</p>
+            <p class="textResult">${quantity} ${quantity===1?"número sorteado":"números sorteados"} de ${min} até ${max}.</p>
         </div>
     `
 }
 
-checkNumbers()
 for(let i=0; i<input.length; i++){input[i].addEventListener("keyup", ()=>{filterInput(i)}, false)}
-for(let i=0; i<input.length; i++){input[i].addEventListener("keyup", checkNumbers, false)}
 button.addEventListener("click", raffle, false)

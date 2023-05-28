5a14cd898a46b04963902b5b40b2f28203f7a21e
LiliKaori <lilikaori10@gmail.com>
2023-05-28 01:27:11 -0300

    Filtrar números e melhorar função raffle

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 "use strict";
 const input = document.getElementsByTagName("input");
-const inputMin = document.getElementById("numMin");
-const inputMax = document.getElementById("numMax");
-const button = document.getElementsByTagName("button");
+const arrayInput = Array.from(input);
+const button = document.getElementsByTagName("button")[0];
+const divResult = document.getElementsByClassName("result");
+function filterInput(i) {
+    const filteredValue = input[i].value.replaceAll(/[\D]/gim, "").replaceAll(/^[0]+/gim, "");
+    input[i].value = filteredValue;
+}
 function checkNumbers() {
-    const min = parseInt(inputMin.value);
-    const max = parseInt(inputMax.value);
-    if (!min || !max) {
-        button[0].disabled = true;
-    }
-    else {
-        button[0].disabled = false;
-    }
-    console.log(button[0]);
+    const empty = arrayInput.find(input => input.value === "");
+    button.disabled = empty ? true : false;
+}
+function raffle() {
+    const values = arrayInput.map((input) => parseInt(input.value));
+    // const quantity = values[0]
+    const numbers = values.slice(1);
+    const min = Math.min(...numbers);
+    const max = Math.max(...numbers);
+    const result = Math.floor(Math.random() * (min - max) + max);
+    input[1].value = min.toString();
+    input[2].value = max.toString();
+    divResult[0].innerHTML += `
+        <div>
+            <h2>${result}</h2>
+            <p class="textResult">1º resultado</p>
+        </div>
+    `;
 }
 checkNumbers();
 for (let i = 0; i < input.length; i++) {
+    input[i].addEventListener("keyup", () => { filterInput(i); }, false);
+}
+for (let i = 0; i < input.length; i++) {
     input[i].addEventListener("keyup", checkNumbers, false);
 }
-function raffle() {
-    const min = parseInt(inputMin.value);
-    const max = parseInt(inputMax.value);
-    const result = Math.floor(Math.random() * (max - min) + min);
-    console.log(result);
-}
+button.addEventListener("click", raffle, false);
--- script.ts
+++ script.ts
@@ -1,28 +1,41 @@
     const input = document.getElementsByTagName("input") as HTMLCollectionOf<HTMLInputElement>
-    const inputMin = document.getElementById("numMin") as HTMLInputElement
-    const inputMax= document.getElementById("numMax") as HTMLInputElement
-    const button = document.getElementsByTagName("button") as HTMLCollectionOf<HTMLButtonElement>
+    const arrayInput : HTMLInputElement[] = Array.from(input)  
+    const button = document.getElementsByTagName("button")[0] as HTMLButtonElement
+    const divResult = document.getElementsByClassName("result") as HTMLCollectionOf<HTMLDivElement>
 
-function checkNumbers(){
-    const min : number = parseInt(inputMin.value)
-    const max : number = parseInt(inputMax.value)
-
-    if(!min || !max){
-        button[0].disabled = true
-    } else {
-        button[0].disabled = false
-    }
-    console.log(button[0])
+function filterInput(i:number) : void {
+    const filteredValue : string = input[i].value.replaceAll(/[\D]/gim,"").replaceAll(/^[0]+/gim, "")   
+    input[i].value = filteredValue
 }
-checkNumbers()
 
-for(let i=0; i<input.length; i++){input[i].addEventListener("keyup", checkNumbers, false)}
+function checkNumbers(): void{
+    const empty = arrayInput.find(input=>input.value==="")
 
+    button.disabled = empty ? true : false
+}
 
-function raffle(): void{
-    const min : number = parseInt(inputMin.value)
-    const max : number = parseInt(inputMax.value)
+function raffle(): void{    
+    const values = arrayInput.map((input)=>parseInt(input.value))
+    // const quantity = values[0]
+    const numbers = values.slice(1)
     
-    const result : number = Math.floor(Math.random() * (max - min) + min)
-    console.log(result)
+    const min : number = Math.min(...numbers)
+    const max : number = Math.max(...numbers)
+
+    const result : number = Math.floor(Math.random() * (min - max) + max)
+
+    input[1].value = min.toString()
+    input[2].value = max.toString()
+
+    divResult[0].innerHTML += `
+        <div>
+            <h2>${result}</h2>
+            <p class="textResult">1º resultado</p>
+        </div>
+    `
 }
+
+checkNumbers()
+for(let i=0; i<input.length; i++){input[i].addEventListener("keyup", ()=>{filterInput(i)}, false)}
+for(let i=0; i<input.length; i++){input[i].addEventListener("keyup", checkNumbers, false)}
+button.addEventListener("click", raffle, false)

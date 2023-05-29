a79892449c92180ed0b8d86fd6625923a85629bb
LiliKaori <lilikaori10@gmail.com>
2023-05-29 01:16:27 -0300

    Incluir permissão de repetir número

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 "use strict";
 const input = document.getElementsByTagName("input");
 const arrayInput = Array.from(input);
-const button = document.getElementsByTagName("button")[0];
+const button = document.getElementById("startButton");
 const divResult = document.getElementsByClassName("result");
+const checkRepeat = document.getElementById("checkRepeat");
 function filterInput(i) {
     const filteredValue = input[i].value.replaceAll(/[\D]/gim, "").replaceAll(/^[0]+/gim, "");
     input[i].value = filteredValue;
@@ -14,9 +15,10 @@ function checkNumbers() {
     const values = arrayInput.map((input) => parseInt(input.value));
     const quantity = values[0];
     const numbers = values.slice(1);
+    const repeat = checkRepeat.getAttribute("repeat");
     const min = Math.min(...numbers);
     const max = Math.max(...numbers);
-    if (quantity && quantity > max - min + 1) {
+    if (!repeat && quantity && quantity > max - min + 1) {
         const alert = document.getElementById("alert");
         alert.innerHTML = `Só pode sortear até ${max - min + 1} ${max - min + 1 == 1 ? "número" : "números"}`;
         button.disabled = true;
@@ -27,20 +29,40 @@ function checkNumbers() {
         button.disabled = false;
     }
 }
+function onOffRepeat() {
+    const repeat = checkRepeat.getAttribute("repeat");
+    if (repeat) {
+        checkRepeat.removeAttribute("repeat");
+        checkRepeat.style.background = "var(--gray)";
+        checkRepeat.innerText = "Não";
+    }
+    else {
+        checkRepeat.setAttribute("repeat", "repeat");
+        checkRepeat.style.background = "var(--blue2)";
+        checkRepeat.innerText = "Sim";
+    }
+    console.log(checkRepeat);
+}
 function raffle() {
     const values = arrayInput.map((input) => parseInt(input.value));
     const quantity = values[0];
     const numbers = values.slice(1);
+    const attributes = Array.from(checkRepeat.attributes).map((attr) => attr.name);
+    const repeat = attributes.indexOf("repeat") === -1 ? false : true;
     const min = Math.min(...numbers);
     const max = Math.max(...numbers);
     let result = [];
     while (result.length < quantity) {
         const sort = Math.floor((Math.random() * (max - min + 1)) + min);
-        if (result.indexOf(` ${sort.toString()}`) === -1) {
+        if (!repeat) {
+            if (result.indexOf(` ${sort.toString()}`) === -1) {
+                result.push(` ${sort.toString()}`);
+            }
+        }
+        else {
             result.push(` ${sort.toString()}`);
         }
     }
-    // const result : number = Math.floor(Math.random() * (min - max) + max)
     input[1].value = min.toString();
     input[2].value = max.toString();
     divResult[0].innerHTML += `
@@ -54,3 +76,4 @@ for (let i = 0; i < input.length; i++) {
     input[i].addEventListener("keyup", () => { filterInput(i); }, false);
 }
 button.addEventListener("click", raffle, false);
+checkRepeat.addEventListener("click", onOffRepeat, false);
--- script.ts
+++ script.ts
@@ -1,7 +1,8 @@
     const input = document.getElementsByTagName("input") as HTMLCollectionOf<HTMLInputElement>
     const arrayInput : HTMLInputElement[] = Array.from(input)  
-    const button = document.getElementsByTagName("button")[0] as HTMLButtonElement
+    const button = document.getElementById("startButton") as HTMLButtonElement
     const divResult = document.getElementsByClassName("result") as HTMLCollectionOf<HTMLDivElement>
+    const checkRepeat = document.getElementById("checkRepeat") as HTMLButtonElement
 
 function filterInput(i:number) : void {
     const filteredValue : string = input[i].value.replaceAll(/[\D]/gim,"").replaceAll(/^[0]+/gim, "")   
@@ -15,26 +16,45 @@ function checkNumbers(): void{
     button.disabled = empty ? true : false
     const values = arrayInput.map((input)=>parseInt(input.value))
     const quantity = values[0]
-    const numbers = values.slice(1)
+    const numbers = values.slice(1)    
+    const repeat = checkRepeat.getAttribute("repeat")
     
     const min : number = Math.min(...numbers)
     const max : number = Math.max(...numbers)
 
-    if(quantity && quantity>max-min+1){
-        const alert = document.getElementById("alert") as HTMLParagraphElement
-        alert.innerHTML=`Só pode sortear até ${max-min+1} ${max-min+1==1?"número":"números"}`
-        button.disabled = true
+    
+        if(!repeat && quantity && quantity>max-min+1){
+            const alert = document.getElementById("alert") as HTMLParagraphElement
+            alert.innerHTML=`Só pode sortear até ${max-min+1} ${max-min+1==1?"número":"números"}`
+            button.disabled = true
+        }else{
+            const alert = document.getElementById("alert") as HTMLParagraphElement
+            alert.innerHTML=""
+            button.disabled = false
+        }
+}
+
+function onOffRepeat(){
+    const repeat = checkRepeat.getAttribute("repeat")
+    
+    if(repeat){
+        checkRepeat.removeAttribute("repeat")
+        checkRepeat.style.background="var(--gray)"
+        checkRepeat.innerText="Não"
     }else{
-        const alert = document.getElementById("alert") as HTMLParagraphElement
-        alert.innerHTML=""
-        button.disabled = false
+        checkRepeat.setAttribute("repeat", "repeat")
+        checkRepeat.style.background="var(--blue2)"
+        checkRepeat.innerText="Sim"
     }
+    console.log(checkRepeat)
 }
 
 function raffle(): void{    
     const values = arrayInput.map((input)=>parseInt(input.value))
     const quantity = values[0]
     const numbers = values.slice(1)
+    const attributes = Array.from(checkRepeat.attributes).map((attr)=>attr.name)
+    const repeat= attributes.indexOf("repeat")===-1 ? false : true
     
     const min : number = Math.min(...numbers)
     const max : number = Math.max(...numbers)
@@ -43,12 +63,14 @@ function raffle(): void{
 
     while(result.length<quantity){
        const sort = Math.floor((Math.random() * (max - min + 1)) + min)
+       if(!repeat){
+        if(result.indexOf(` ${sort.toString()}`)===-1)
+        {result.push(` ${sort.toString()}`)}
+       }else{
+        result.push(` ${sort.toString()}`)
+       }
        
-       if(result.indexOf(` ${sort.toString()}`)===-1)
-       {result.push(` ${sort.toString()}`)}
     }
-
-    // const result : number = Math.floor(Math.random() * (min - max) + max)
 
     input[1].value = min.toString()
     input[2].value = max.toString()
@@ -63,3 +85,4 @@ function raffle(): void{
 
 for(let i=0; i<input.length; i++){input[i].addEventListener("keyup", ()=>{filterInput(i)}, false)}
 button.addEventListener("click", raffle, false)
+checkRepeat.addEventListener("click", onOffRepeat, false)
